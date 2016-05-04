546e8283207075ce188d340ff2e8a0649b79b799
Sondre <sondrf14@uia.no>
2016-05-04 12:02:03 +0200

    buzzer module

---
+++ buzzermodule.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+BeepPin = 11 
+
+def setup():
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(BeepPin, GPIO.OUT)
+GPIO.output(BeepPin, GPIO.HIGH)
+
+def loop():
+while True:
+GPIO.output(BeepPin, GPIO.LOW)
+time.sleep(0.1)
+GPIO.output(BeepPin, GPIO.HIGH)
+time.sleep(0.1)
+
+def destroy():
+GPIO.output(BeepPin, GPIO.HIGH) 
+GPIO.cleanup() 
+
+if __name__ == '__main__': 
+print 'Press Ctrl+C to exit'
+setup()
+try:
+loop()
+except KeyboardInterrupt: 
+destroy()
