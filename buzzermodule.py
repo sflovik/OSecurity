bc90b0b8ec95c372f608f65f04fcdd0d6050d082
Sondre <sondrf14@uia.no>
2016-05-04 12:23:45 +0200

    buzzer module update

--- buzzermodule.py
+++ buzzermodule.py
@@ -4,25 +4,25 @@ import time
 BeepPin = 11 
 
 def setup():
-GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(BeepPin, GPIO.OUT)
-GPIO.output(BeepPin, GPIO.HIGH)
+	GPIO.setmode(GPIO.BOARD) 
+	GPIO.setup(BeepPin, GPIO.OUT)
+	GPIO.output(BeepPin, GPIO.HIGH)
 
 def loop():
-while True:
-GPIO.output(BeepPin, GPIO.LOW)
-time.sleep(0.1)
-GPIO.output(BeepPin, GPIO.HIGH)
-time.sleep(0.1)
+	while True:
+		GPIO.output(BeepPin, GPIO.LOW)
+		time.sleep(0.1)
+		GPIO.output(BeepPin, GPIO.HIGH)
+		time.sleep(0.1)
 
 def destroy():
-GPIO.output(BeepPin, GPIO.HIGH) 
-GPIO.cleanup() 
+	GPIO.output(BeepPin, GPIO.HIGH) 
+	GPIO.cleanup() 
 
-if __name__ == '__main__': 
-print 'Press Ctrl+C to exit'
+	if __name__ == '__main__': 
+		print 'Press Ctrl+C to exit'
 setup()
 try:
-loop()
+	loop()
 except KeyboardInterrupt: 
-destroy()
+	destroy()
