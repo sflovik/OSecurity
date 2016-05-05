8a01de61cf799e97d7288d64ea5e1a0501ab9e59
Sondre <sondrf14@uia.no>
2016-05-05 11:52:54 +0200

    skrivefeil rettet

--- terminalscripts.py
+++ terminalscripts.py
@@ -11,7 +11,7 @@ import subprocess as sp
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
-extProc = ""
+
 def spStart():
 	extProc = sp.Popen(['python','buzzermodule.py']) # Starter subprocess for buzzermodul
 	status = sp.Popen.poll(extProc) # status none  
