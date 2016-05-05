ad603a56b04910a051da128f28a6fc9c66c127eb
Ricky Omland <RickyOmland@rickys-air.uia.no>
2016-05-05 14:51:10 +0200

    Lastet opp java/python kode for serverkommunikasjon

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket              
+soc = socket.socket()        
+host = "localhost"
+port = 2004               
+soc.bind((host, port))       
+soc.listen(5)               
+while True:
+    conn, addr = soc.accept()    
+    print ("Got connection from",addr)
+    msg = conn.recv(1024)
+    print (msg)
+if ( msg == "Hello Server" ):
+    print("Hii everyone")
+else:
+    print("Go away")
