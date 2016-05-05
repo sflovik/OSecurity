4432488456006507247ca57bb6d06a93cc2a05d1
Sondre <sondrf14@uia.no>
2016-05-05 13:19:16 +0200

    mute

--- terminalscripts.py
+++ terminalscripts.py
@@ -11,7 +11,7 @@ import subprocess as sp
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
-muted = False 
+muted = True 
 
 def startupInput():
 	print "Hi! Would you like the buzzer to be active for this session? y/n"
