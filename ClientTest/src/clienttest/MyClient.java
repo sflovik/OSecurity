e6a2c8cf13c8a041ce3f52bf54f59d04339475ab
cfthorne <cfthorne@hotmail.com>
2016-05-11 15:15:08 +0200

    fungerende client

--- ClientTest/src/clienttest/MyClient.java
+++ ClientTest/src/clienttest/MyClient.java
@@ -1,16 +1,51 @@
- import java.io.*;  
+    import java.io.*;  
     import java.net.*; 
     public class MyClient {
+    private static boolean mute = true;
+    private static final String sendMute = "mute";
+    private static final String sendActive = "active"; 
     public static void main(String[] args) {  
+    
+    
+    }
+    public static void setMute(){
+        if (mute) {
+        try{      
+   
+            Socket soc=new Socket("25.31.197.199", 2004);  
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
 
-    try{      
-    Socket soc=new Socket("192.168.137.131", 2004);  
-    DataOutputStream dout=new DataOutputStream(soc.getOutputStream());  
-    dout.writeUTF("Hello");
-    dout.flush();
-    dout.close();  
-    soc.close();
-    }catch(Exception e){
-        e.printStackTrace();}  
-    }  
-  }
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            out.print(sendMute);
+            out.close();
+            in.close();
+            soc.close();
+            }catch(UnknownHostException e){
+                System.err.println("Unknown Host.");
+            }catch (IOException e) {
+                System.err.println("Couldn't get I/O for connection");
+            }
+        }
+        else {
+         try{      
+   
+            Socket soc=new Socket("25.31.197.199", 2004);  
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            out.print(sendActive);
+            out.close();
+            in.close();
+            soc.close();
+            }catch(UnknownHostException e){
+                System.err.println("Unknown Host.");
+            }catch (IOException e) {
+                System.err.println("Couldn't get I/O for connection");
+            }   
+            
+    }
+    }
+    }
+    
