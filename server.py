b3c8305b06659558c34632011d54c9121a0598bc
Sondre <sondrf14@uia.no>
2016-05-11 16:34:11 +0200

    tilpasset server script til linux

--- server.py
+++ server.py
@@ -1,23 +1,23 @@
 import socket
-import win32com.client as comclt
+import subprocess
+import os
 soc = socket.socket()        
-host = "25.31.197.199"
+host = "25.53.39.221"
 port = 2004               
 soc.bind((host, port))       
 soc.listen(5)
-#os.system("gnome-terminal -e 'bash -c \"python terminalscripts.py; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"python terminalscripts.py; exec bash\"'")
 while 1:
     conn, addr = soc.accept()    
     print ("Got connection from",addr)
     msg = conn.recv(1024).decode()
     print (msg)
     if ( msg == "mute" ):
-        wsh = comclt.Dispatch("WScript.Shell")
-        wsh.SendKeys("n")
-        wsh.SendKeys("{ENTER}")
+        subprocess.call([xdotool], "key", ["n"])
+        subprocess.call(["xdotool", "key", "Return"])
+       
     elif ( msg == "active"):
-        wsh = comclt.Dispatch("WScript.Shell")
-        wsh.SendKeys("y")
-        wsh.SendKeys("{ENTER}")
+         subprocess.call(["xdotool", "key", "y"])
+         subprocess.call(["xdotool", "key", "Return"])
     else:
         print ("Invalid input from client...")
