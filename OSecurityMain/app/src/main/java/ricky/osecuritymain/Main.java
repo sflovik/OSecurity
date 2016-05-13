9e10d670382b48b836272be44c5e458c1e1a1aec
sflovik <sondrf14@gmail.com>
2016-05-13 12:40:53 +0200

    Integrert client funksjonalitet med app gui

--- OSecurityMain/app/src/main/java/ricky/osecuritymain/Main.java
+++ OSecurityMain/app/src/main/java/ricky/osecuritymain/Main.java
@@ -10,9 +10,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ToggleButton;
+import java.io.*;
+import java.net.*;
 
 public class Main extends AppCompatActivity {
-
+    private static boolean mute = true;
+    private static final String sendMute = "mute";
+    private static final String sendActive = "active";
+    private static final String sendArming = "arming";
+    private static final String sendDisarm = "disarming";
     ToggleButton toggle;
     EditText ed;
 
@@ -38,6 +44,90 @@ public class Main extends AppCompatActivity {
         });
     }
 
+    public static void setArming() {
+        try {
+
+            Socket soc = new Socket("25.95.63.199", 2004);
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(sendArming);
+            out.print(sendArming);
+            out.close();
+            in.close();
+            soc.close();
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown Host.");
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for connection");
+
+        }
+    }
+    public static void setDisarming() {
+        try{
+
+            Socket soc=new Socket("25.95.63.199", 2004);
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(sendDisarm);
+            out.print(sendDisarm);
+            out.close();
+            in.close();
+            soc.close();
+        }catch(UnknownHostException e){
+            System.err.println("Unknown Host.");
+        }catch (IOException e) {
+            System.err.println("Couldn't get I/O for connection");
+
+        }
+    }
+
+    public static void setMute() {
+        if (mute) {
+            try {
+
+                Socket soc = new Socket("25.95.63.199", 2004);
+                PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println(sendMute);
+                out.print(sendMute);
+                out.close();
+                in.close();
+                soc.close();
+            } catch (UnknownHostException e) {
+                System.err.println("Unknown Host.");
+            } catch (IOException e) {
+                System.err.println("Couldn't get I/O for connection");
+
+            }
+        } else {
+            try {
+
+                Socket soc = new Socket("25.95.63.199", 2004);
+                PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println(sendActive);
+                out.print(sendActive);
+                out.close();
+                in.close();
+                soc.close();
+            } catch (UnknownHostException e) {
+                System.err.println("Unknown Host.");
+            } catch (IOException e) {
+                System.err.println("Couldn't get I/O for connection");
+                System.out.println(sendMute);
+                System.out.println(sendActive);
+            }
+
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -65,11 +155,11 @@ public class Main extends AppCompatActivity {
             // TODO Auto-generated method stub
             //toggle.toggle();
             if ( ed.getText().toString().equalsIgnoreCase("1")) {
-
+                setArming();
                 toggle.setTextOff("TOGGLE ON");
                 toggle.setChecked(true);
             } else if ( ed.getText().toString().equalsIgnoreCase("0")) {
-
+                setDisarming();
                 toggle.setTextOn("TOGGLE OFF");
                 toggle.setChecked(false);
 
