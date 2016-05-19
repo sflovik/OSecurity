f5d38b816fb0afbfeccb1fad0d45d5c75252c9ae
sflovik <sondrf14@gmail.com>
2016-05-19 15:23:26 +0200

    Documented application logic and some XML-documentation of GUI

--- OSecurityMain/app/src/main/java/ricky/osecuritymain/Main.java
+++ OSecurityMain/app/src/main/java/ricky/osecuritymain/Main.java
@@ -1,5 +1,13 @@
+/**
+ * Main class for GruppeX's OSecurity android application
+ * Contains all java logic to be executed.
+ * @authors Sondre Flovik, Ricky Omland, Christian Fredrik Thorne
+ * @date May 2016
+ */
 package ricky.osecuritymain;
-
+/**
+ * All imports for the project
+ */
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
@@ -19,6 +27,9 @@ import android.widget.ToggleButton;
 import java.io.*;
 import java.net.*;
 
+/**
+ * Start of main class, set all variables
+ */
 public class Main extends AppCompatActivity {
     private static boolean mute = false;
     int counter = 1;
@@ -26,19 +37,26 @@ public class Main extends AppCompatActivity {
     private static final String sendActive = "active";
     private static final String sendArming = "arming";
     private static final String sendDisarm = "disarming";
-  //  private static final int REQUEST_EXTERNAL_STORAGE = 1;
-  //  private static String[] PERMISSIONS_STORAGE = {
-    //        Manifest.permission.READ_EXTERNAL_STORAGE,
-      //      Manifest.permission.WRITE_EXTERNAL_STORAGE };
+  //  private static final int REQUEST_EXTERNAL_STORAGE = 1; unused
+  //  private static String[] PERMISSIONS_STORAGE = { unused
+    //        Manifest.permission.READ_EXTERNAL_STORAGE, unused
+      //      Manifest.permission.WRITE_EXTERNAL_STORAGE }; unused
 
     ToggleButton toggle;
     EditText ed;
 
-
+    /**
+     * When the UI is drawn / created on application start
+     * @param savedInstanceState instance state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = new View(this);
+        // Not good practice, but a temporary solution to allow networking to be executed
+        // in the GUI thread of the application. Forces permission, as opposed to
+        // the good practice of multi-threading in these situations
+        // Can potentially cause instability and application crashes
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
@@ -60,6 +78,8 @@ public class Main extends AppCompatActivity {
         });
     }
     /**
+     * Teimporarily unused - alternative way of granting the application writing permissions
+     * in runtime environment
     public static void verifyStoragePermissions(Activity activity) {
         int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
@@ -71,12 +91,17 @@ public class Main extends AppCompatActivity {
         }
     }
      */
+    /**
+     * Method to arm the system, sends the static sendArming variable to the terminal
+     */
     public static void setArming() {
         try {
-
+            // Connects to the terminals socket connection
+            // Could / should have made the variables static and global
+            // to prevent code duplication on several methods
             Socket soc = new Socket("25.95.63.199", 2004);
             PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-
+            // Sets up a BufferedReader to get input and send data to socket host
             BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             System.out.println(sendArming);
@@ -91,6 +116,11 @@ public class Main extends AppCompatActivity {
 
         }
     }
+
+    /**
+     * Method to disarm the system
+     * Same functionality as in setArming(), except another variable is sent to the socket host
+     */
     public static void setDisarming() {
         try{
 
@@ -112,6 +142,12 @@ public class Main extends AppCompatActivity {
         }
     }
 
+    /**
+     * Method to set wether buzzer should be active or not on the terminal
+     * Sends either sendMute variable or sendActive variable, depending on the state
+     * of the global "mute" variable, true/false
+     * Functionality same as in previous methods
+     */
     public static void setMute() {
         if (mute) {
             try {
@@ -177,6 +213,11 @@ public class Main extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Method belonging to the xml-button for the buzzer, sets global variable
+     * depending on click / state in XML
+     * @param v
+     */
         public void muteClick(View v) {
             if (mute) {
                 mute = false;
@@ -185,6 +226,19 @@ public class Main extends AppCompatActivity {
                 mute = true;
             }
         }
+
+    /**
+     * Method belonging to the xml-button for arming/disarming
+     * Counter variable starts at 1, which is an unarmed system
+     * Tries to arm the system as long as variable is 1, and sets counter ++ (counter=2)
+     * then sleeps for 1,5 seconds to wait for terminal to perform script startup, before
+     * it calls setMute() to complete initializing the terminal script
+     *
+     * If the system is active, the counter equals 2, and the setDisarming() method is called
+     * to send a KeyboardInterrupt to the terminal's terminal window to disarm the system
+     * Counter is set back to 1, so that the next click will activate the system again.
+     * @param v
+     */
         public void onClick(View v) {
         long millis = 1500;
 
